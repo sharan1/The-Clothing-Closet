7e79759b2f5d2e609f65cf3d9911cdcf6db279bb
Sharan Girdhani <sharan.girdhani@gmail.com>
2017-04-02 12:16:59 -0400

    Minor issues

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=clothing_closet',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
